4e31365e05fed0db0b759e0a82d0477fcef720bf
electrikbox <electrikbox@icloud.com>
2023-12-21 14:03:39 +0100

    change User to Review at line 106

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -12,8 +12,9 @@ from models.user import User
 # GET all reviews from a place
 # ============================================================================
 
-@app_views.route('/places/<place_id>/reviews',
-                 methods=['GET'], strict_slashes=False)
+
+@app_views.route("/places/<place_id>/reviews",
+                 methods=["GET"], strict_slashes=False)
 def get_all_reviews_from_place(place_id):
     """get all reviews from place"""
     place = storage.get(Place, place_id)
@@ -23,7 +24,8 @@ def get_all_reviews_from_place(place_id):
     reviews = storage.all(Review).values()
 
     list_reviews = [
-        review.to_dict() for review in reviews if reviews.place_id == place_id]
+        review.to_dict() for review in reviews if review.place_id == place_id
+    ]
 
     return jsonify(list_reviews)
 
@@ -31,7 +33,9 @@ def get_all_reviews_from_place(place_id):
 # GET 1 review
 # ============================================================================
 
-@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route("/reviews/<review_id>",
+                 methods=["GET"], strict_slashes=False)
 def get_review(review_id):
     """get review by id"""
     review = storage.get(Review, review_id)
@@ -44,8 +48,9 @@ def get_review(review_id):
 # DELETE a review
 # ============================================================================
 
-@app_views.route('reviews/<review_id>',
-                 methods=['DELETE'], strict_slashes=False)
+
+@app_views.route("reviews/<review_id>",
+                 methods=["DELETE"], strict_slashes=False)
 def delete_review(review_id):
     """delete review by id"""
     review = storage.get(Review, review_id)
@@ -60,8 +65,9 @@ def delete_review(review_id):
 # CREATE a review
 # ============================================================================
 
-@app_views.route('/places/<place_id>/reviews',
-                 methods=['POST'], strict_slashes=False)
+
+@app_views.route("/places/<place_id>/reviews",
+                 methods=["POST"], strict_slashes=False)
 def create_review(place_id):
     """create new review"""
 
@@ -72,18 +78,19 @@ def create_review(place_id):
     data = request.get_json()
 
     if not data:
-        abort(400, 'Not a JSON')
+        abort(400, "Not a JSON")
 
-    if 'user_id' not in data:
-        abort(400, 'Missing user_id')
+    if "user_id" not in data:
+        abort(400, "Missing user_id")
 
-    if 'text' not in data:
-        abort(400, 'Missing text')
+    if "text" not in data:
+        abort(400, "Missing text")
 
-    user = storage.get(User, data['user_id'])
+    user = storage.get(User, data["user_id"])
     if not user:
         abort(404)
 
+    # data['place_id'] = place_id
     review = Review(**data)
     review.save()
     return jsonify(review.to_dict()), 201
@@ -92,19 +99,20 @@ def create_review(place_id):
 # UPDATE a user
 # ============================================================================
 
-@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route("/reviews/<review_id>", methods=["PUT"], strict_slashes=False)
 def update_review(review_id):
     """updtae user by id"""
-    review = storage.get(User, review_id)
+    review = storage.get(Review, review_id)
     if not review:
         abort(404)
 
     data = request.get_json()
 
     if not data:
-        abort(400, 'Not a JSON')
+        abort(400, "Not a JSON")
 
-    ignore_key = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
+    ignore_key = ["id", "user_id", "place_id", "created_at", "updated_at"]
 
     for key, value in data.items():
         if key not in ignore_key:
